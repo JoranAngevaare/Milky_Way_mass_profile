c29ea092860c50e533f9fb69b4082a22f90d92b3
Joran Angevaare <jorana@nikhef.nl>
2022-05-04 15:53:02 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+readme = open('README.md').read()
+
+setup(name='Milky_Way_mass_profile',
+      version='0.0.0',
+      long_description=readme,
+      author='Marius Cautun',
+      url='https://github.com/MariusCautun/Milky_Way_mass_profile',
+      license='GNU GENERAL PUBLIC LICENSE',
+      py_modules=['Cautun20_galpy_potential', 'Cautun20_galpy_potential'],
+      zip_safe=False)
